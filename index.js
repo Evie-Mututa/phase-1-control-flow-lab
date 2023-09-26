3550a6a3a61d9d89f52813be065f40dc404dd21a
Evie-Mututa <evemututa29@gmail.com>
2023-09-26 07:26:49 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue) {
   // Write your code here!
+  if (someValue > 2500) {
+    return "No can do.";
+  } else if (someValue > 400 && someValue < 2000) {
+    return "I will gladly take your thirty bucks.";
+  } else if (someValue <= 400) {
+    return "This one is on me!";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  let isNYC = city == "NYC" ? "Ok, sounds good." : "No go.";
+  return isNYC;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  let tipOffer;
+  switch (tip) {
+    case "generous":
+      tipOffer = "Thank you so much.";
+      break;
+    case "not generous":
+      tipOffer = "Thank you.";
+      break;
+    default:
+      tipOffer = "Bye.";
+  }
+  return tipOffer;
 }
